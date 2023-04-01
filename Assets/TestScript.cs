f8e964fca1625841c2f7d844768e3626f1f91a57
rPbl3yH <44815308+rPbl3yH@users.noreply.github.com>
2023-04-01 18:59:18 +0500

    add notification

--- Assets/TestScript.cs
+++ Assets/TestScript.cs
@@ -7,7 +7,7 @@ public class TestScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        print("Hello world");
     }
 
     // Update is called once per frame
