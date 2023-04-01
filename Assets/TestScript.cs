f037129a3366440d654d7ae9a2ea548a91c9ff9a
rPbl3yH <44815308+rPbl3yH@users.noreply.github.com>
2023-04-01 19:03:12 +0500

    feat: print I am player

--- Assets/TestScript.cs
+++ Assets/TestScript.cs
@@ -7,7 +7,7 @@ public class TestScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        print("I am player");
     }
 
     // Update is called once per frame
